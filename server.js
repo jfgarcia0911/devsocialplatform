1919e50d4ed33bd6755a9e39c719f7493c0bf84e
Garcia, John Francis <john.francis.garcia@accenture.com>
2023-12-26 13:47:29 +0800

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
-
+const connectDB = require('./config/db')
+const userRoute = require('./routes/api/users')
+const authRoute = require('./routes/api/auth')
+const profileRoute = require('./routes/api/profile')
+const postRoute = require('./routes/api/posts')
 const app = express()
 
+//Connect database
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}))
+
 app.get('/', (req,res) => res.send('API Running'))
+
+//Define Routes
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/profile', profileRoute)
+app.use('/api/posts', postRoute)
 
 const PORT = process.env.PORT || 5000;
 
