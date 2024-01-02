73ad17307c7cc6106b370799ca2e542e8742ba27
Garcia, John Francis <john.francis.garcia@accenture.com>
2024-01-02 21:07:47 +0800

    prepared for deployment

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const userRoute = require('./routes/api/users')
 const authRoute = require('./routes/api/auth')
 const profileRoute = require('./routes/api/profile')
 const postRoute = require('./routes/api/posts')
+const path = require('path')
+
 const app = express()
 
 //Connect database
@@ -16,7 +18,8 @@ app.use(express.json({extended: false}))
 // Enable CORS for all routes
 // app.use(cors());
 
-app.get('/', (req,res) => res.send('API Running'))
+//Comment-out not to be included
+// app.get('/', (req,res) => res.send('API Running'))
 
 
 //Define Routes
@@ -24,6 +27,16 @@ app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/profile', profileRoute)
 app.use('/api/posts', postRoute)
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
